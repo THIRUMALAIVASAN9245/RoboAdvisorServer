f7e923069c043c8b460fc6a056748e68d29a7eba
Thirumalaivasan S <THIRUMALAIVASAN9245@GMAIL.COM>
2020-12-08 21:41:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message":"Hello Thirumalai"}
+
+@app.get("/sample")
+def home():
+    return {"message":"Hello Thirumalai"}
+
+@app.post("/train")
+async def train(params: TrainParameters):
+    print("Model Training Started")
+    print(params)
+    return {"message":"train"}
+
+@app.post("/predict")
+async def predict(data:RoboAdviserSample):
+    print("Predicting")
+    print(data)
+    return {"message":"predict"}
